6592ae2b9067e65c53f0a7755ee35668a75f901c
EstefaniFelix <estefaniescola1@gmail.com>
2024-06-24 20:21:19 +0000

    save

--- trabalho/app/(tabs)/sobre.tsx
+++ trabalho/app/(tabs)/sobre.tsx
@@ -19,11 +19,20 @@ export default function App() {
                     <Text style={styles.text} >Experimente o poder da localização em tempo real a qualquer momento com nosso app.
                     </Text>
 
-                    <Image style={styles.img} source={require('../../assets/images/img2.png')} />
+                    <Image style={styles.img1} source={require('../../assets/images/img2.png')} />
+
+                    <Text style={styles.text2} >Localização Atual </Text>
 
                     <Image style={styles.img} source={require('../../assets/images/img3.png')} />
+
+                    <Text style={styles.text3} >Latitude: 0.00 </Text>
+                    <Text style={styles.text3} >Longitude: 0.00 </Text>
+                    <TouchableOpacity style={styles.button2} onPress={() => setPage('home')}>
+                            <Text style={styles.buttonText}> Obter Localização </Text>
+                        </TouchableOpacity>
+
                         <TouchableOpacity style={styles.button} onPress={() => setPage('home')}>
-                            <Text style={styles.buttonText}> Clique aqui </Text>
+                            <Text style={styles.buttonText}> Home </Text>
                         </TouchableOpacity>
                
                 </View>
@@ -43,6 +52,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#CAE9FF',
         alignItems: 'center',
+        justifyContent:'center',
 
     },
 
@@ -50,31 +60,50 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: 'bold',
         color: '#1B4965',
-        marginTop: 10,
-        marginRight: 'auto',
+        marginRight: 305,
     },
 
     text: {
-        fontSize: 19,
+        fontSize: 20,
         color: '#1E1E1E',
         fontWeight: 'bold',
+        textAlign: 'justify',
+        margin:30,
     },
-
+    text2: {
+        fontSize: 18,
+        color: '#1E1E1E',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        marginRight:305,
+        margin:10,
+    },
+    text3: {
+        fontSize: 15,
+        color: '#1E1E1E',
+        fontWeight: 'bold',
+        textAlign: 'justify',
+        marginRight:305,
+       
+    },
     button: {
         backgroundColor: '#5FA8D3',
         padding: 10,
-        borderRadius: 12,
-        position: 'absolute',
-        transform: [{ scaleX: -1 }],
+        borderRadius: 12,    
+        alignItems: 'center',
 
+    },
+    button2: {
+        backgroundColor: '#5FA8D3',
+        padding: 10,
+        borderRadius: 12,    
+        alignItems: 'center',
+        marginBottom:10,
     },
     buttonText: {
         color: '#CAE9FF',
         fontSize: 18,
         fontWeight: 'bold',
-        position: 'absolute',
-        transform: [{ scaleX: -1 }],
-
     },
 
     img: {
@@ -82,8 +111,16 @@ const styles = StyleSheet.create({
         height: 200,
         borderColor: '#000',
         borderRadius: 15,
-        marginBottom: 100,
-
+        alignItems: 'center',
+        marginBottom:10,
+    },
+    img1: {
+        width: 250,
+        height: 100,
+        borderColor: '#000',
+        borderRadius: 15,
+        alignItems: 'center',
+        marginBottom:10,
     },
 
 });
